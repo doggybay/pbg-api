b39ce8d8ecaa4f6104dc0d791e9b4e8f71e9af5e
Julio Miranda <julio.miranda12@gmail.com>
2021-10-23 02:51:09 +0100

    adding price

--- db/migrations/01_plates.js
+++ db/migrations/01_plates.js
@@ -1,10 +1,10 @@
-
 exports.up = function(knex) {
   return knex.schema.createTable('plates', table => {
     table.increments();
     table.string('name').notNullable();
     table.string('description').notNullable();
     table.string('picture').unique().notNullable();
+    table.integer('price').unique();
     table.timestamps(true, true);
   })
   
--- db/seeds/01_plates.js
+++ db/seeds/01_plates.js
@@ -9,16 +9,19 @@ exports.seed = function(knex) {
           name: "molletes",
           description: "pan dulce con dulce",
           picture: "https://res.cloudinary.com/platosbygloria/image/upload/v1632088621/plates/molletes_bjahmu.jpg",
+          price: "9.99",
         },
         {
           name: "tacos",
           description: "tortilla de maiz con carne de res",
           picture: "https://res.cloudinary.com/platosbygloria/image/upload/v1632088618/plates/tacos_c7t3mu.jpg",
+          price: "9.99",
         },
         {
           name: "pescado con arroz",
           description: "pescado y arroze blanco",
           picture: "https://res.cloudinary.com/platosbygloria/image/upload/v1632088618/plates/pescadoconarroz_ax1co4.jpg",
+          price: "9.99",
         },
       ]);
     });
