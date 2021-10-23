2fa4c7ec531a85614439f886bbb98273933d9016
Julio Miranda <julio.miranda12@gmail.com>
2021-10-23 22:31:17 +0100

    making routes

--- routes/customers.js
+++ routes/customers.js
@@ -4,5 +4,9 @@ const router = express.Router();
 const customersController = require('../controllers/customers')
 
 router.get('/customers', customersController.getAllCustomers);
+router.get('/customers/:id', customersController.getOneCustomer);
+router.post('/customers', customersController.addOneCustomer);
+router.patch('/customers/:id', customersController.updateOneCustomer);
+router.delete('/customers/:id', customersController.deleteOneCustomer);
 
 module.exports = router;
