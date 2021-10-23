a0199a8d11c6693ece9f3c17e01eab374025785e
Julio Miranda <julio.miranda12@gmail.com>
2021-10-23 22:27:33 +0100

    adding customer routes

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const port = process.env.PORT || 8000;
 const cors = require('cors');
 const logger = require('morgan');
 const { Model } = require('objection');
 const knexInstance = require('./db/knex');
 
+// routes
 const plates = require('./routes/plates');
+const customers = require('./routes/customers');
 
 Model.knex(knexInstance);
 
@@ -18,10 +19,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/api', plates);
+app.use('/api', customers);
 
 
 app.get('/', (req, res) => {
-  res.send('Server is up.....');
+  res.send(`Server is up....`);
 })
 
-module.exports = { app }
+module.exports = { app };
