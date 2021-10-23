d2e5d05ed785daed1854ffff3f4f100f4e1cbf1d
Julio Miranda <julio.miranda12@gmail.com>
2021-10-23 22:28:10 +0100

    making controllers

--- controllers/customers.js
+++ controllers/customers.js
@@ -1,7 +1,60 @@
 const Customer = require('../models/Customer');
-
+const Address = require('../models/Address');
 
 exports.getAllCustomers = async (req, res) => {
-  const customers = await Customer.query().withGraphFetched('[addresses]');
-  res.json(customers)
+  const customers = await Customer.query().withGraphFetched('[address]');
+  
+  res.json(customers);
+};
+
+exports.getOneCustomer = async (req, res) => {
+  const customerId = req.params.id;
+  const customer = await Customer.query().findById(customerId);
+
+  res.json(customer);
+};
+
+exports.addOneCustomer = async (req, res) => {
+  const {first_name, last_name, email, phone, st_address, opt_address, city, state, zip_code} = req.body;
+  const address = {
+    st_address,
+    opt_address,
+    city,
+    state,
+    zip_code
+  };
+  const customer = {
+    first_name,
+    last_name,
+    email,
+    phone
+  };
+
+  const newCustomer = await Customer.query().insert(customer).returning('*');
+
+  const formattedAddress = {
+    ...address,
+    user_id: newCustomer.id
+  };
+
+  await Address.query().insert(formattedAddress);
+
+  const custWithAddr = await Customer.query().findById(newCustomer.id).withGraphFetched('[address]');
+
+  res.json(custWithAddr);
+}
+
+exports.updateOneCustomer = async (req, res) => {
+  
+}
+
+exports.deleteOneCustomer = async (req, res) => {
+  const customerId = req.parms.id;
+
+  const deletedAddress = await Address.query().select('customer_id').where('addresses.customer_id', '=', customerId).del().returning('*');
+  const deletedCustomer = await Customer.query().deleteById(customerId).returning('*');
+
+  const newDelCust = { ...deletedCustomer, address: [deletedAddress] };
+
+  res.json(newDelCust);
 }
--- controllers/plates.js
+++ controllers/plates.js
@@ -1,6 +1,39 @@
 const Plate = require('../models/Plate');
 
-exports.getAllPlates = (req, res) => {
-  Plate.query().then(plates => res.json(plates));
+exports.getAllPlates = async (req, res) => {
+  const plates = await Plate.query();
   
+  res.json(plates);
+};
+
+exports.getOnePlate = async (req, res) => {
+  const plateId = req.parms.id;
+  const plate = await Plate.query().findById(plateId);
+
+  res.json(plate);
+};
+
+exports.addOnePlate = async (req, res) => {
+  const plate = { ...req.body }
+
+  const newPlate = await Plate.query().insert(plate).returning('*');
+  
+  res.json(newPlate)
+};
+
+exports.updateOnePlate = async (req, res) => {
+  const plateToUpdate = { ...req.body };
+  const plateId = req.parms.id;
+
+  const updatedPlate = await Plate.query().findById(plateId).patch(plateToUpdate).returning('*');
+  
+  res.json(updatedPlate);
+};
+
+exports.deleteOnePlate = async (req, res) => {
+  const plateId = req.parms.id;
+
+  const deletedPlate = await Plate.query().deleteById(plateId).returning('*');
+
+  res.json(deletedPlate);
 };
