2e8a66d3108fd1a8c238ca03c0f6f86b6f2f7998
Julio Miranda <julio.miranda12@gmail.com>
2021-10-23 02:51:31 +0100

    creating orders

--- db/migrations/04_orders.js
+++ db/migrations/04_orders.js
@@ -1,8 +1,14 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('orders', table => {
+    table.increments();
+    table.integer('customer_id').notNullable();
+    table.integer('plate_id').notNullable();
+    table.integer('total').notNullable();
+    table.integer('quantity').notNullable();
+    table.timestamps(true, true);
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable('orders');
 };
--- db/migrations/05_users.js
+++ db/migrations/05_users.js
@@ -4,7 +4,6 @@ exports.up = function(knex) {
     table.string('first_name').notNullable();
     table.string('last_name').notNullable();
     table.string('email').notNullable();
-    table.string('phone');
     table.timestamps(true, true);
   });
 };
