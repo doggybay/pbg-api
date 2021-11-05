842834e08d196a210fb13ef1892a6da12db837e6
Julio Miranda <julio.miranda12@gmail.com>
2021-11-05 08:26:29 +0100

    updating db url

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ let connectionString = process.platform === 'win32' ? 'postgres://db_access:free
   module.exports = {
     development: {
         client: 'pg',
-        connection: connectionString,
+        connection: process.env.DATABASE_URL,
         migrations: {
             directory: __dirname + '/db/migrations',
           },
